bb432037557a31022c0984c973a421c959a0189e
mohammad mahmoudi <mm580486@gmail.com>
2017-04-26 00:03:15 +0430

    add version method and extend process class

---
+++ lib/tehranjs/cli.rb
@@ -0,0 +1,34 @@
+require "thor"
+require "thor/actions"
+require "thor_plus/actions"
+
+
+module CsvUtil
+  # The Command Line Interface (CLI) for the gem.
+  class CLI < Thor
+    include Thor::Actions
+    include ThorPlus::Actions
+
+    package_name Identity.version_label
+    namespace :default
+
+    register(
+      Process,
+      "process", "process server",
+      "Process for run server",
+      hide: true
+    )
+
+
+    def initialize args = [], options = {}, config = {}
+      super args, options, config
+    end
+
+    desc "-v, [--version]", "Version."
+    map %w[-v --version] => :version
+    def version
+      say Identity.version_label
+    end
+
+  end
+end
