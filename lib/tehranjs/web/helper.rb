4e52bb61fec82a8c07cad53c3f6dd83bd33cc32c
mohammad mahmoudi <mm580486@gmail.com>
2017-04-26 14:20:48 +0430

    Add image tag

--- lib/tehranjs/web/helper.rb
+++ lib/tehranjs/web/helper.rb
@@ -11,12 +11,14 @@ def content_tag(element=:div,content=nil,single=false,attr={})
     return tag
 end
 
+def image_tag asset_path
+    return content_tag(:img,'',single=true,{src: "/public/assets/img/#{asset_path}",width: "65" })
+end
+
 def stylesheets_link_tag asset_path
    return content_tag(:link,'',single=true,{href: "/public/assets/css/#{asset_path}" ,media: "all" ,rel: "stylesheet"})
 end
 
 def javascript_link_tag asset_path
-   return content_tag(:script,'',single=false,{href: "/public/assets/js/#{asset_path}"})
+   return content_tag(:script,'',single=false,{src: "/public/assets/js/#{asset_path}"})
 end
-
-
