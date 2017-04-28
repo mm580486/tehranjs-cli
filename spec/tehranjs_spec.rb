b875eecb82ce7d9480eb01bdf9e96f6fa36d13ce
mohammad mahmoudi <mm580486@gmail.com>
2017-04-28 23:16:24 +0430

    Fix gem version

--- spec/tehranjs_spec.rb
+++ spec/tehranjs_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 RSpec.describe Tehranjs do
   it "has a version number" do
-    expect(Tehranjs::VERSION).not_to be nil
+    expect(Tehranjs::Identity.version).not_to be nil
   end
 
   it "does something useful" do
