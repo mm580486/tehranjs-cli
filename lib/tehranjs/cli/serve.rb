ac749ad90fa3fb73a747abc90568e7d215b18cee
mohammad mahmoudi <mm580486@gmail.com>
2017-04-29 21:57:44 +0430

    add lang parameter for choose lang

--- lib/tehranjs/cli/serve.rb
+++ lib/tehranjs/cli/serve.rb
@@ -1,8 +1,7 @@
 require "webrick"
+require 'i18n'
 require "tehranjs/web/controller"
 require "tehranjs/web/servlet"
-
-
 module Tehranjs
   class CLI < Thor
     class Serve < Thor::Group
@@ -18,19 +17,25 @@ module Tehranjs
                    desc: "Server Port",
                    type: :numeric, default: 2525
 
+      class_option :lang,
+                   aliases: "-l",
+                   desc: "Server lang",
+                   type: :string, default: 'en'
+
       def Serve
+        I18n.load_path = Dir[::File.join(::File.dirname(__FILE__),"..","web","views", 'locals','*.yml')]
+        I18n.default_locale = options[:lang].to_sym
+        say("#{options[:lang].to_sym} localiziation")
+        say("Server run at http://127.0.0.1:#{options[:port]}")
+        server = WEBrick::HTTPServer.new(:Port => options[:port])
+        server.mount '/public', WEBrick::HTTPServlet::FileHandler,::File.join(::File.dirname(__FILE__),"..", 'web','views','public')
+        server.mount "/", Tehranjs::Web::Servlet
 
-server = WEBrick::HTTPServer.new(:Port => options[:port])
+        trap("INT") {
+            server.shutdown
+        }
 
-server.mount '/public', WEBrick::HTTPServlet::FileHandler,::File.join(::File.dirname(__FILE__),"..", 'web','views','public')
-server.mount "/", Tehranjs::Web::Servlet
-
-trap("INT") {
-    server.shutdown
-}
-
-server.start
-
+        server.start
 
       end
 
