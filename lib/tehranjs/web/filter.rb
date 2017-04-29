a3a7ea4176c6c556507cacf4e16742a3c86b79ca
mohammad mahmoudi <mm580486@gmail.com>
2017-04-29 20:20:43 +0430

    add filter class (before_filter)

---
+++ lib/tehranjs/web/filter.rb
@@ -0,0 +1,24 @@
+ module Tehranjs
+module Web
+ module Filter
+  def before_filter name
+    @@filter = name
+  end
+
+  def method_added name
+    return if @filtering # Don't add filters to original_ methods
+    return if @@filter == name # Don't filter filters
+    return if name == :initialize
+
+    @filtering = true
+
+    alias_method :"original_#{name}", name
+    define_method name do |*args|
+      self.send @@filter, name
+      self.send :"original_#{name}", *args
+    end
+    @filtering = false
+  end
+end
+end
+end
